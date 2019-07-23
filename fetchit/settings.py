697d0b028137fa00a0d1ffc3a6b2f3eba4a503f8
Abhishek-PHP <abhishek-php@webappclouds.com>
2019-07-23 14:45:27 +0530

    database setting added

--- fetchit/settings.py
+++ fetchit/settings.py
@@ -75,8 +75,16 @@ WSGI_APPLICATION = 'fetchit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'worker_provider',
+        'USER': 'root',
+        'PASSWORD':'',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        # 'OPTIONS':{
+        #     'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
+
+        #  }
     }
 }
 
